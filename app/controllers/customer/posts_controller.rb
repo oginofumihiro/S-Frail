b37af62e5e9f30250ea97e9675d51c72ef3563ad
oginofumihiro <fumi1127ogino@gmail.com>
2021-05-13 14:30:03 +0000

    update

--- app/controllers/customer/posts_controller.rb
+++ app/controllers/customer/posts_controller.rb
@@ -5,6 +5,7 @@ class Customer::PostsController < ApplicationController
   end
 
   def show
+     @post = Post.find(params[:id])
   end
 
   def new
@@ -18,9 +19,13 @@ class Customer::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to customer_posts_path
   end
 
   def destroy
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7c0e149dae8ac4d0fcbf7195b6150e2c69d08ab5cfba7b8ce98c02da4154171f4f33f2c322fa1c9b7581b2061b8d9cd7ed9b589c0e19eec121de814758b0d8f8'
