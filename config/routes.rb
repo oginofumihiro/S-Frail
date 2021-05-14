7ad8c9762c32d6d269b1913bff9766db4073bb95
oginofumihiro <fumi1127ogino@gmail.com>
2021-05-14 15:11:01 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   namespace :customer do
     resources :posts,only: [:index, :show, :edit, :update, :new, :create, :destroy ]
   end
-  devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  devise_for :customers
 
   get 'top' => 'homes#top'
 
