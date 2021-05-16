9c739e4732481f5c78723d894c37c18e47c2241e
oginofumihiro <fumi1127ogino@gmail.com>
2021-05-16 08:47:04 +0000

    update

--- app/controllers/customer/users_controller.rb
+++ app/controllers/customer/users_controller.rb
@@ -1,11 +1,11 @@
 class Customer::UsersController < ApplicationController
   def show
      @customer = Customer.find(params[:id])
-    # @post_images = @user.post_images.page(params[:page]).reverse_order
+     @profile_images = @customer.profile_image
   end
-  
+
   def set_user
     @user = User.find([:id])
   end
-  
+
 end
