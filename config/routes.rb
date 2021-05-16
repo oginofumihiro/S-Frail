12c6b9a4a0db75218c902018c88fd93e4e9237a8
oginofumihiro <fumi1127ogino@gmail.com>
2021-05-16 13:05:50 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :customers
   namespace :customer do
     resources :posts,only: [:index, :show, :edit, :update, :new, :create, :destroy ]
-    resources :users, only: [:show]
+    resources :users, only: [:show, :edit, :update, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
 
