9029562690d0df5e516ccb880a1f0a7bd67c1be9
oginofumihiro <fumi1127ogino@gmail.com>
2021-05-17 08:56:39 +0000

    update

--- app/controllers/customer/posts_controller.rb
+++ app/controllers/customer/posts_controller.rb
@@ -14,8 +14,11 @@ class Customer::PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to customer_posts_path
+    if @post.save
+       redirect_to customer_posts_path
+    else
+       render :new
+    end
   end
 
   def edit
