72e7958f474b00309b0f9bd691b055b457752a15
oginofumihiro <fumi1127ogino@gmail.com>
2021-05-18 02:46:33 +0000

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-    # before_action :authenticate_customers_post_user!,except: [:top]
+    before_action :authenticate_customer!,except: [:top]
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_valid,:email, :password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_valid,:email, :password, :profile_image])
   end
 
   def after_sign_in_path_for(resource)
