1e6faf2c1c534e922fe39f5d70514b94c3d55c47
oginofumihiro <fumi1127ogino@gmail.com>
2021-05-22 12:26:13 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :customers,only: [:index]
+    get "out" => "customers#out"
     resources :genres,only: [:index, :create, :edit, :update, :destroy]
   end
 
@@ -15,11 +16,13 @@ Rails.application.routes.draw do
   namespace :customer do
     resources :posts,only: [:index, :show, :edit, :update, :new, :create, :destroy]
     resources :users, only: [:show, :edit, :update, :destroy]
+    get "out" => "users#out"
+    patch 'withdraw' => "users#withdraw"
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
 
-  get 'top' => 'homes#top'
+  get "top" => "homes#top"
 
   get "homes/about", to: "homes#about"
 end
