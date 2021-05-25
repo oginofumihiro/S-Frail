3cf36379991ea27f1ae12fb4bc8a42a4412b3712
oginofumihiro <fumi1127ogino@gmail.com>
2021-05-25 08:04:58 +0000

    update

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,14 @@
+class Admin::CustomersController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @customers = Customer.all
+    @customers = Customer.all.page(params[:page]).per(10)
+  end
+
+  private
+
+  def customers_params
+    params.require(:customer).permit(:first_name,:last_name,:postal_code,:residence,:phone_number,:email,:is_valid, :page)
+  end
+end
