470abdad405612c5a675e20b742d9db71ff760e8
oginofumihiro <fumi1127ogino@gmail.com>
2021-05-28 02:24:09 +0000

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create(email: 'sfrail@1127.com', password: 'password')
+Admin.create(email: 'sfrail@1127.com', password: 'password') #管理者のメールアドレスとパスワード
 Genre.create(name: "運動")
-#管理者のメールアドレスとパスワード
