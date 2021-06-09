fce70634f86565c4e9279b80511a6371c462ee80
oginofumihiro <fumi1127ogino@gmail.com>
2021-06-09 08:01:56 +0000

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
 //= require popper
-//= require bootstrap-sprockets
 
+//= require jquery
 //= require rails-ujs
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
