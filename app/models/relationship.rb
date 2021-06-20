d76072f0b91946dd6927f0c04143cd8b7253e3f2
oginofumihiro <fumi1127ogino@gmail.com>
2021-06-20 05:46:48 +0000

    update

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,6 +1,6 @@
 class Relationship < ApplicationRecord
   belongs_to :customer
-  belongs_to :follow, class_name: 'Customer'
+  belongs_to :follow, class_name: 'Customer', foreign_key: "customer_id"
   validates :customer_id, presence: true
   validates :follow_id, presence: true
 end
