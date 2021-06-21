e2f9967eeed29c02057f0a9ed5c2bad7c6aaf6f6
oginofumihiro <fumi1127ogino@gmail.com>
2021-06-21 12:40:40 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,89 +10,91 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_614_121_323) do
-  create_table 'admins', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_admins_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_admins_on_reset_password_token', unique: true
+ActiveRecord::Schema.define(version: 2021_06_14_121323) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-  create_table 'customers', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.string 'last_name'
-    t.string 'first_name'
-    t.string 'last_name_kana'
-    t.string 'first_name_kana'
-    t.string 'postal_code'
-    t.text 'address'
-    t.string 'telephone_number'
-    t.boolean 'is_valid', default: true, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'profile_image_id'
-    t.index ['email'], name: 'index_customers_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_customers_on_reset_password_token', unique: true
+  create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "postal_code"
+    t.text "address"
+    t.string "telephone_number"
+    t.boolean "is_valid", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "profile_image_id"
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
-  create_table 'favorites', force: :cascade do |t|
-    t.integer 'post_id'
-    t.integer 'post_image_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'customer_id'
+  create_table "favorites", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "customer_id"
   end
 
-  create_table 'genres', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'inquiries', force: :cascade do |t|
-    t.string 'name'
-    t.string 'message'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "inquiries", force: :cascade do |t|
+    t.string "name"
+    t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'post_comments', force: :cascade do |t|
-    t.text 'comment'
-    t.integer 'customer_id'
-    t.integer 'post_image_id'
-    t.integer 'post_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "customer_id"
+    t.integer "post_image_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'posts', force: :cascade do |t|
-    t.integer 'genre_id'
-    t.string 'name'
-    t.string 'post_image_id'
-    t.text 'introduction'
-    t.boolean 'is_active'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'customer_id'
-    t.datetime 'start_time'
+  create_table "posts", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.string "post_image_id"
+    t.text "introduction"
+    t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "customer_id"
+    t.datetime "start_time"
   end
 
-  create_table 'relationships', force: :cascade do |t|
-    t.integer 'customer_id'
-    t.integer 'follow_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[customer_id follow_id], name: 'index_relationships_on_customer_id_and_follow_id', unique: true
-    t.index ['customer_id'], name: 'index_relationships_on_customer_id'
-    t.index ['follow_id'], name: 'index_relationships_on_follow_id'
+  create_table "relationships", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "follow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id", "follow_id"], name: "index_relationships_on_customer_id_and_follow_id", unique: true
+    t.index ["customer_id"], name: "index_relationships_on_customer_id"
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
   end
+
 end
